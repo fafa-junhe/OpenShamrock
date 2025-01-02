40872dc53b19973f1e3ff3064759f28cf162d56a
fafa_junhe <842355358@qq.com>
2025-01-02 21:20:48 +0800

    Update AioListener.kt
    update pong + battery

--- xposed/src/main/java/moe/fuqiuluo/shamrock/remote/service/listener/AioListener.kt
+++ xposed/src/main/java/moe/fuqiuluo/shamrock/remote/service/listener/AioListener.kt
@@ -26,6 +26,11 @@ import moe.fuqiuluo.shamrock.utils.PlatformUtils
 import moe.fuqiuluo.shamrock.utils.PlatformUtils.QQ_9_0_8_VER
 import java.util.ArrayList
 import kotlin.collections.HashMap
+import android.content.Context
+import android.content.Context.BATTERY_SERVICE
+import android.os.BatteryManager
+import android.content.Intent
+import mqq.app.MobileQQ
 
 internal object AioListener : IKernelMsgListener {
     override fun onRecvMsg(msgList: ArrayList<MsgRecord>) {
@@ -35,6 +40,20 @@ internal object AioListener : IKernelMsgListener {
             msgList.forEach {
                 handleMsg(it)
             }
+        }
+    }
+    private fun getBatteryPercentage(context: Context): Int {
+        return if (Build.VERSION.SDK_INT >= 21) {
+            val bm = context.getSystemService(BATTERY_SERVICE) as BatteryManager
+            bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        
+        } else {
+            val iFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+            val batteryStatus: Intent = context.registerReceiver(null, iFilter)
+            val level = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale = batteryStatus?.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+            val batteryPct = level / scale.toDouble()
+            (batteryPct * 100).toInt()
         }
     }
 
@@ -65,7 +84,7 @@ internal object AioListener : IKernelMsgListener {
             if (rawMsg.isEmpty()) return
 
             if (ShamrockConfig.aliveReply() && rawMsg == "ping") {
-                MessageHelper.sendMessageWithoutMsgId(record.chatType, record.peerUin.toString(), "pong", { _, _ -> })
+                MessageHelper.sendMessageWithoutMsgId(record.chatType, record.peerUin.toString(), "pong" + getBatteryPercentage(MobileQQ.getContext()), { _, _ -> })
             } /*else if (ShamrockConfig.aliveReply() && rawMsg == ".shamrock.at_me") {
                 MessageHelper.sendMessageWithoutMsgId(record.chatType, record.peerUin.toString(), "[CQ:at,qq=${record.senderUin}]", { _, _ -> })
             }*/
