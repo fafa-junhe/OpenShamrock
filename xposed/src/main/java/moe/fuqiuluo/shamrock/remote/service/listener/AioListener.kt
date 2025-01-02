c670c9d33b6971d464bf2b763aca7493e85616f1
fafa_junhe <842355358@qq.com>
2025-01-02 21:36:18 +0800

    Update AioListener.kt

--- xposed/src/main/java/moe/fuqiuluo/shamrock/remote/service/listener/AioListener.kt
+++ xposed/src/main/java/moe/fuqiuluo/shamrock/remote/service/listener/AioListener.kt
@@ -31,6 +31,7 @@ import android.content.Context.BATTERY_SERVICE
 import android.os.BatteryManager
 import android.content.Intent
 import mqq.app.MobileQQ
+import android.content.IntentFilter
 
 internal object AioListener : IKernelMsgListener {
     override fun onRecvMsg(msgList: ArrayList<MsgRecord>) {
@@ -44,18 +45,24 @@ internal object AioListener : IKernelMsgListener {
     }
     private fun getBatteryPercentage(context: Context): Int {
         return if (Build.VERSION.SDK_INT >= 21) {
-            val bm = context.getSystemService(BATTERY_SERVICE) as BatteryManager
+            val bm = context.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
             bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
-        
         } else {
             val iFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
-            val batteryStatus: Intent = context.registerReceiver(null, iFilter)
-            val level = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
-            val scale = batteryStatus?.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
-            val batteryPct = level / scale.toDouble()
-            (batteryPct * 100).toInt()
+            val batteryStatus: Intent? = context.registerReceiver(null, iFilter)
+            
+            val level = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
+            val scale = batteryStatus?.getIntExtra(BatteryManager.EXTRA_SCALE, -1) ?: -1
+            
+            if (level != -1 && scale != -1) {
+                val batteryPct = level.toDouble() / scale.toDouble()
+                (batteryPct * 100).toInt()
+            } else {
+                -1 // Return -1 or some default value if level or scale is invalid
+            }
         }
     }
+
 
     private suspend fun handleMsg(record: MsgRecord) {
         try {
